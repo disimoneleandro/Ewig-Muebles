2edd1dc92f4b9adadbda0e7bf8d8038a94a17e2d
Leandro Di Simone <disimoneleandro@gmail.com>
2023-06-10 19:31:50 -0300

    Se agrega envio de form

--- src/components/FormularioContacto.js
+++ src/components/FormularioContacto.js
@@ -1,29 +1,65 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios';
 import "../styles/FormularioContacto.css"
 
-const FormularioContacto = () => {
+const FormularioContacto = (props) => {
+
+  const initialForm = {
+    nombre : '',
+    email : '',
+    comentario : ''
+  }
+
+  const [sending, setSending] = useState(false);
+  const [msg, setMsg] = useState('');
+  const [formData, setFormData] = useState(initialForm);
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormData(oldData => ({
+      ...oldData,
+      [name]: value
+    }));
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setMsg('');
+    setSending(true)
+
+    const response = await axios.post('http://localhost:3000/api/contacto', formData);
+    setSending(false);
+    setMsg(response.data.message);
+    if( response.data.error === false ) {
+      setFormData(initialForm)
+    }
+  }
+
   return (
     <div className="div-principal">
       <div className="div-h4">
         <h4 className="h4">PEDI TU PRESUPUESTO</h4>
       </div>
-      <div className="div-form">
+      <form action="/contacto" method="post" className="div-form" onSubmit={handleSubmit}>
         <div class="form-floating">
-          <input type="text" class="form-control input-form" id="floatingInput" placeholder="Nombre" />
+          <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} class="form-control input-form" id="floatingInput" placeholder="Nombre" />
           <label for="floatingInput">Nombre</label>
         </div>
         <div class="form-floating">
-          <input type="email" class="form-control input-form" id="floatingPassword" placeholder="Email" />
+          <input type="email" name="email" value={formData.email} onChange={handleChange} class="form-control input-form" id="floatingPassword" placeholder="Email" />
           <label for="floatingPassword">Email</label>
         </div>
         <div class="form-floating">
-          <textarea class="form-control input-form textarea" placeholder="Leave a comment here" id="floatingTextarea2"></textarea>
-          <label for="floatingTextarea2">Comentario / Presupuesto</label>
+          <textarea name="mensaje" value={formData.mensaje} onChange={handleChange} class="form-control input-form textarea" placeholder="Leave a comment here" id="floatingTextarea2"></textarea>
+          <label for="floatingTextarea2">Mensaje</label>
         </div>
         <div class="div-btn">
           <button  type="submit" class="btn btn-primary">ENVIAR</button>
         </div>
-      </div>
+        {sending ? <p>Enviando...</p> : null}
+        {msg ? <p>{msg}</p> : null}
+      </form>
     </div>
   )
 }
--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import NovedadItem from '../components/novedades/NovedadItem';
 
-const NovedadesPage = () => {
+const NovedadesPage = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [novedades, setNovedades] = useState([]);
+
+  useEffect(() => {
+    const cargarNovedades = async () => {
+      setLoading(true);
+      const response = await axios.get('http://localhost:3000/api/novedades');
+      setNovedades(response.data);
+      setLoading(false);
+    };
+
+    cargarNovedades();
+  }, []);
+
   return (
     <div className="div-novedades-page">
-    <h2>PAGINA DE NOVEDADES</h2>
-    <h2>PAGINA DE NOVEDADES</h2>
-    <h2>PAGINA DE NOVEDADES</h2>
+      <h3>Novedades</h3>
+      {loading ? (
+        <p>Cargando...</p>
+        ) : (
+          novedades.map(item => <NovedadItem key={item.id}
+          title={item.titulo}
+          subtitle={item.subtitulo}
+          imagen={item.image}
+          body={item.cuerpo} />)
+      )}
     </div>
   )
 }
