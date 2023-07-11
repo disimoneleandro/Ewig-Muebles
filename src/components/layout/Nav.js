f4a18f4933721edf741a6085a5b205dde6aa5af4
Leandro Di Simone <disimoneleandro@gmail.com>
2023-07-11 01:53:34 -0300

    Modificacion btn

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -6,7 +6,7 @@ import BurgerBoton from '../BurgerBoton';
 
 const Nav = () => {
 
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
 
   const showToggle = () => {
     setShow(!show);
